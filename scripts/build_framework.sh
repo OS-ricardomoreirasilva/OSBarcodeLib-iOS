7b47f1fe90ac6e844c5dad5f02c0bb80425cd10c
OS-ricardomoreirasilva <ricardo.moreira.silva@outsystems.com>
2024-04-04 10:14:25 +0100

    again

--- scripts/build_framework.sh
+++ scripts/build_framework.sh
@@ -23,6 +23,6 @@ BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
 xcodebuild -create-xcframework \
 -framework './scripts/build/OSBarcodeLib.framework-iphonesimulator.xcarchive/Products/Library/Frameworks/OSBarcodeLib.framework' \
 -framework './scripts/build/OSBarcodeLib.framework-iphoneos.xcarchive/Products/Library/Frameworks/OSBarcodeLib.framework' \
--output './scripts/build/OSBarcodeLib.xcframework'
+-output './OSBarcodeLib.xcframework'
 
-zip -r ./scripts/build/OSBarcodeLib.zip ./scripts/build/OSBarcodeLib.xcframework
+zip -r ./scripts/build/OSBarcodeLib.zip ./OSBarcodeLib.xcframework
